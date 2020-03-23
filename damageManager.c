dd2c2c1130ed77dbfc1c1a7cf39347ff1ea7244a
DoktorSAS <62484597+DoktorSAS@users.noreply.github.com>
2020-03-23 01:10:52 +0100

    Add files via upload

---
+++ damageManager.c
@@ -0,0 +1,12 @@
+damageManager(){
+	self endon( "disconnect" );
+	level endon( "game_ended" );
+	for(;;){	
+        self waittill("damage", damage, attacker);
+		if ( isSubStr(attacker getCurrentWeapon(), "cheytac") || isSubStr(attacker getCurrentWeapon(), "barrett") || isSubStr(attacker getCurrentWeapon(), "wa2000") || isSubStr(attacker getCurrentWeapon(), "fal") || isSubStr(attacker getCurrentWeapon(), "m21") || isSubStr(attacker getCurrentWeapon(), "m40a3") || isSubStr(attacker getCurrentWeapon(), "remington700") || isSubStr(attacker getCurrentWeapon(), "dragunov") || isSubStr(attacker getCurrentWeapon(), "mk14") || isSubStr(attacker getCurrentWeapon(), "l96a1") || isSubStr(self getCurrentWeapon(), "dsr") || isSubStr(attacker getCurrentWeapon(), "ballista") || isSubStr(attacker getCurrentWeapon(), "fall") || isSubStr(attacker getCurrentWeapon(), "mosin") || isSubStr(attacker getCurrentWeapon(), "msr") || isSubStr(attacker getCurrentWeapon(), "svu") || isSubStr(attacker getCurrentWeapon(), "xpr" )){
+			Damage = 999;
+		}else{
+			self.health = self.health + damage;
+		}	
+	}
+}
